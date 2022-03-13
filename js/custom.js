34cf87f0523628ae1cb0442a570e456f15c7fb07
Sajjad Ahmad <73050766+sajjadah964@users.noreply.github.com>
2022-03-13 21:46:04 +0500

    page is added

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 100) {
+            $("nav").addClass("active");
+        } else {
+            $("nav").removeClass("active");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $(".heading .single-heading").on("click", function() {
+        var _this = $(this);
+
+        $(".single-heading").removeClass("active");
+        _this.addClass("active");
+
+        var tagid = _this.data("tag");
+
+        $(".single-content").removeClass("active");
+        $('#'+tagid).addClass("active");
+    })
+})
